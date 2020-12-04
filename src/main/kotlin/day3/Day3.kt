a99a3f7c1f12e5620e4a6bfd59025af4c8250d1a
Fernando <fernando@diversius.com>
2020-12-04 22:37:37 +0100

    Add day 3 solution, part 2

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -17,13 +17,33 @@ private fun buildMap(lines: List<String>): MapOfTrees {
     return map
 }
 
+private val stepThreeRightOneDown: (Coordinate) -> Coordinate = { Coordinate(it.x + 3, it.y + 1) }
+private val stepOneRightOneDown: (Coordinate) -> Coordinate = { Coordinate(it.x + 1, it.y + 1) }
+private val stepFiveRightOneDown: (Coordinate) -> Coordinate = { Coordinate(it.x + 5, it.y + 1) }
+private val stepSevenRightOneDown: (Coordinate) -> Coordinate = { Coordinate(it.x + 7, it.y + 1) }
+private val stepOneRightTwoDown: (Coordinate) -> Coordinate = { Coordinate(it.x + 1, it.y + 2) }
+
 fun main() {
     val lines = fetchData()
     val map = buildMap(lines)
-    val journey = Journey(map) { Coordinate(it.x + 3, it.y + 1) }
-    journey.findTrees()
+    val journey = Journey(map)
+    val foundTrees = journey.findTrees(stepThreeRightOneDown)
+    println(foundTrees.size)
 
-    print(journey.foundTrees.size)
+    val foundTreesOneRightOneDown = journey.findTrees(stepOneRightOneDown).size
+    val foundTreesThreeRightOneDown = journey.findTrees(stepThreeRightOneDown).size
+    val foundTreesFiveRightOneDown = journey.findTrees(stepFiveRightOneDown).size
+    val foundTreesSevenRightOneDown = journey.findTrees(stepSevenRightOneDown).size
+    val foundTreesOneRightTwoDown = journey.findTrees(stepOneRightTwoDown).size
+    val result = listOf(
+        foundTreesOneRightOneDown.toBigInteger(),
+        foundTreesThreeRightOneDown.toBigInteger(),
+        foundTreesFiveRightOneDown.toBigInteger(),
+        foundTreesSevenRightOneDown.toBigInteger(),
+        foundTreesOneRightTwoDown.toBigInteger(),
+    ).reduce { acc, i -> acc * i }
+
+    println("$foundTreesOneRightOneDown * $foundTreesThreeRightOneDown * $foundTreesFiveRightOneDown * $foundTreesSevenRightOneDown * $foundTreesOneRightTwoDown = $result")
 }
 
 class MapOfTrees {
@@ -60,11 +80,13 @@ open class Coordinate(val x: Int, val y: Int)
 
 class TreeCoordinate(x: Int, y: Int, var hasTree: Boolean) : Coordinate(x, y)
 
-class Journey(private val map: MapOfTrees, val stepFn: (Coordinate) -> Coordinate) {
-    private var currentPosition = Coordinate(0, 0)
-    val foundTrees = mutableListOf<Coordinate>()
+class Journey(private val map: MapOfTrees) {
+    private lateinit var currentPosition: Coordinate
 
-    fun findTrees() {
+    fun findTrees(stepFn: (Coordinate) -> Coordinate): MutableList<Coordinate> {
+        currentPosition = Coordinate(0, 0)
+        val foundTrees = mutableListOf<Coordinate>()
+
         do {
             if (map.hasTree(currentPosition)) {
                 foundTrees.add(currentPosition)
@@ -72,6 +94,8 @@ class Journey(private val map: MapOfTrees, val stepFn: (Coordinate) -> Coordinat
 
             currentPosition = stepFn(currentPosition)
         } while (!hasReachedBottom())
+
+        return foundTrees
     }
 
     private fun hasReachedBottom(): Boolean {
