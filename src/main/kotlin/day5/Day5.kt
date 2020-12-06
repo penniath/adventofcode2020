f409ebd8408edc1bd81f72a2622c935268f2c261
Fernando <fernando@diversius.com>
2020-12-06 10:42:40 +0100

    Add day 5 solution, part 2

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -21,6 +21,17 @@ fun main() {
     val max = boardingPasses.maxByOrNull { it.id }
 
     println(max?.id)
+
+    val allSeats = (1 until 127).flatMap { row -> (0..7).map { BoardingPass(row, it) } }
+    val missing = allSeats.minus(boardingPasses)
+    val matches = missing.find { pass ->
+        val previous = boardingPasses.find { it.id == pass.id - 1 }
+        val next = boardingPasses.find { it.id == pass.id + 1 }
+
+        previous != null && next != null
+    }
+
+    println(matches?.id)
 }
 
 data class BoardingPass(val row: Int, val column: Int) {
@@ -28,6 +39,25 @@ data class BoardingPass(val row: Int, val column: Int) {
         get() {
             return row * 8 + column
         }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BoardingPass
+
+        if (row != other.row) return false
+        if (column != other.column) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = row
+        result = 31 * result + column
+        return result
+    }
+
 }
 
 private fun calculateRow(definition: String): Int {
