9d5336190fc28d70021e05af2ec4a3c69ad976de
Fernando <fernando@diversius.com>
2020-12-07 18:05:33 +0100

    Add day 7 solution, part 2

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -1,6 +1,7 @@
 package day7
 
 import util.fetchData
+import java.lang.Integer.parseInt
 
 private fun parseBags(lines: List<String>): List<Bag> {
     return lines.map(::parseBag)
@@ -14,7 +15,11 @@ private fun parseBag(line: String): Bag {
         listOf()
     } else {
         groups[2].split(",")
-            .map { "(\\d+) (.+) bag".toRegex().find(it)!!.groupValues[2] }
+            .map {
+                val content = "(\\d+) (.+) bag".toRegex().find(it)!!.groupValues
+
+                Content(content[2], parseInt(content[1]))
+            }
     }
 
     return Bag(id, children)
@@ -23,10 +28,14 @@ private fun parseBag(line: String): Bag {
 fun countBagsCanContainShinyGold(bags: List<Bag>): List<Bag> {
     val bagsById = bags.map { it.id to it }.toMap()
 
-    return bags.filterIndexed { index, bag ->
-        println(index)
-        bag.canContainBag("shiny gold", bagsById)
-    }
+    return bags.filter { it.canContainBag("shiny gold", bagsById) }
+}
+
+fun countBagsContainingShinyGold(bags: List<Bag>): Int {
+    val bagsById = bags.map { it.id to it }.toMap()
+    val shinyGold = bags.find { it.id == "shiny gold" }!!
+
+    return shinyGold.sumBags(bagsById)
 }
 
 fun main() {
@@ -35,14 +44,27 @@ fun main() {
     val bagsThanCanContainShinyGold = countBagsCanContainShinyGold(bags)
 
     println(bagsThanCanContainShinyGold.size)
+
+    val bagCount = countBagsContainingShinyGold(bags)
+    println(bagCount)
 }
 
-data class Bag(val id: String, val children: List<String>) {
+data class Bag(val id: String, val children: List<Content>) {
 
     fun canContainBag(id: String, bags: Map<String, Bag>): Boolean {
         return children.any {
-            it == id || children.map { bagId -> bags[bagId] }
+            it.id == id || children.map { bag -> bags[bag.id] }
                 .any { bag -> bag?.canContainBag(id, bags) ?: false }
         }
     }
+
+    fun sumBags(bags: Map<String, Bag>): Int {
+        return children.map {
+            val bag = bags[it.id]!!
+
+            it.amount * (1 + bag.sumBags(bags))
+        }.sum()
+    }
 }
+
+data class Content(val id: String, val amount: Int)
--- src/test/kotlin/day7/Day7KtTest.kt
+++ src/test/kotlin/day7/Day7KtTest.kt
@@ -14,17 +14,39 @@ internal class Day7KtTest {
         assertEquals(4, result.size)
     }
 
+    @Test
+    fun countBagsContainingShinyGold() {
+        val bags = createBagsPart2()
+
+        val result = countBagsContainingShinyGold(bags)
+
+        assertEquals(126, result)
+    }
+
     private fun createBags(): List<Bag> {
         val bags = mutableListOf<Bag>()
-        bags.add(Bag("light red", listOf("white bags", "muted yellow")))
-        bags.add(Bag("dark orange", listOf("bright white", "muted yellow")))
-        bags.add(Bag("bright white", listOf("shiny gold")))
-        bags.add(Bag("muted yellow", listOf("shiny gold", "faded blue")))
-        bags.add(Bag("shiny gold", listOf("dark olive", "vibrant plum")))
-        bags.add(Bag("dark olive", listOf("faded blue", "dotted black")))
-        bags.add(Bag("vibrant plum", listOf("faded blue", "dotted black")))
+        bags.add(Bag("light red", listOf(Content("bright white", 1), Content("muted yellow", 2))))
+        bags.add(Bag("dark orange", listOf(Content("bright white", 3), Content("muted yellow", 4))))
+        bags.add(Bag("bright white", listOf(Content("shiny gold", 1))))
+        bags.add(Bag("muted yellow", listOf(Content("shiny gold", 2), Content("faded blue", 9))))
+        bags.add(Bag("shiny gold", listOf(Content("dark olive", 1), Content("vibrant plum", 2))))
+        bags.add(Bag("dark olive", listOf(Content("faded blue", 3), Content("dotted black", 4))))
+        bags.add(Bag("vibrant plum", listOf(Content("faded blue", 5), Content("dotted black", 6))))
         bags.add(Bag("faded blue", listOf()))
         bags.add(Bag("dotted black", listOf()))
+
+        return bags
+    }
+
+    private fun createBagsPart2(): List<Bag> {
+        val bags = mutableListOf<Bag>()
+        bags.add(Bag("shiny gold", listOf(Content("dark red", 2))))
+        bags.add(Bag("dark red", listOf(Content("dark orange", 2))))
+        bags.add(Bag("dark orange", listOf(Content("dark yellow", 2))))
+        bags.add(Bag("dark yellow", listOf(Content("dark green", 2))))
+        bags.add(Bag("dark green", listOf(Content("dark blue", 2))))
+        bags.add(Bag("dark blue", listOf(Content("dark violet", 2))))
+        bags.add(Bag("dark violet", listOf()))
 
         return bags
     }
